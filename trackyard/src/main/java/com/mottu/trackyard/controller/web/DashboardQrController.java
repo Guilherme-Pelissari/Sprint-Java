459813e9a1760f4dbb22fe40faf06971372bd161
guilhermefiap <guipelissari2000@gmail.com>
2025-11-07 18:42:23 -0300

    alterando o gerador e leitor de qrcode

--- trackyard/src/main/java/com/mottu/trackyard/controller/web/DashboardQrController.java
+++ trackyard/src/main/java/com/mottu/trackyard/controller/web/DashboardQrController.java
@@ -1,20 +1,35 @@
 package com.mottu.trackyard.controller.web;
 
+import com.mottu.trackyard.service.PontosLeituraService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.data.domain.PageRequest; // 1. ADICIONE ESTE IMPORT
 
 @Controller
 public class DashboardQrController {
 
+    private final PontosLeituraService pontosLeituraService;
+
+    public DashboardQrController(PontosLeituraService pontosLeituraService) {
+        this.pontosLeituraService = pontosLeituraService;
+    }
+
     @GetMapping("/dashboard/gerar")
     public String gerarPage(Model model) {
-        // se quiser, insira dados no model
-        return "dashboard/gerar"; // templates/dashboard/gerar.html
+        return "dashboard/gerar"; 
     }
 
     @GetMapping("/dashboard/ler")
     public String lerPage(Model model) {
-        return "dashboard/ler"; // templates/dashboard/ler.html
+        
+        // 2. CRIE UM PAGE REQUEST (Ex: página 0, 100 itens)
+        PageRequest pageRequest = PageRequest.of(0, 100); 
+
+        // 3. PASSE O PAGE REQUEST EM VEZ DE 'null'
+        var pontos = pontosLeituraService.getAllPontosLeitura(pageRequest);
+        model.addAttribute("listaDePontos", pontos.getContent());
+        
+        return "dashboard/ler"; 
     }
 }
